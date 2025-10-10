cae42de28debdab9f742edb0e215267aacf59f8c
jeong-min yeo <duwjdduwjd11@gmail.com>
2025-10-10 21:10:08 +0900

    html파일 옮기기

--- 로그인/script4.js
+++
@@ -1,7 +0,0 @@
-const modal = document.getElementById('loginModal');
-const openBtn = document.getElementById('loginBtn');
-const closeBtn = document.querySelector('.close');
-
-openBtn.addEventListener('click', () => modal.style.display = 'block');
-closeBtn.addEventListener('click', () => modal.style.display = 'none');
-window.addEventListener('click', e => { if (e.target === modal) modal.style.display = 'none'; });

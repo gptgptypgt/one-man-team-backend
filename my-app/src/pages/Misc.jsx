d7030174dd82c344df3815fb97c54b845577304e
trisagion2001-creator <trisagion2001@gmail.com>
2025-10-23 17:57:53 +0900

    장바구니 id 중복 버그 수정

--- my-app/src/pages/Misc.jsx
+++ my-app/src/pages/Misc.jsx
@@ -6,42 +6,42 @@ const IMG_BASE = "/misc"
 const PRODUCTS = [ 
   // 모니터
   {
-    id: 1,
+    id: "misc-1",
     img: 'samsung_oled.png', alt: '모니터1',
     spec1: '80.3cm / 32인치', spec2: '240Hz / 평면',
     name: '삼성전자 오디세이 OLED G8 S32FG810',
     price: '1,504,200원',
   },
   {
-    id: 2,
+    id: "misc-2",
     img: 'samsung_g7.png', alt: '모니터2',
     spec1: '80.3cm / 32인치', spec2: '144Hz / 평면',
     name: '삼성전자 오디세이 G7 S32DG700',
     price: '756,460원',
   },
   {
-    id: 3,
+    id: "misc-3",
     img: 'samsung_g5.png', alt: '모니터3',
     spec1: '80cm / 32인치', spec2: '165Hz / 커브드',
     name: '삼성전자 오디세이 G5 S32CG550',
     price: '294,300원',
   },
   {
-    id: 4,
+    id: "misc-4",
     img: 'lg_hd.png', alt: '모니터4',
     spec1: '80cm / 32인치', spec2: '60Hz / 평면',
     name: 'LG전자 울트라HD 32UP830K',
     price: '590,000원',
   },
   {
-    id: 5,
+    id: "misc-5",
     img: 'lg_gear.png', alt: '모니터5',
     spec1: '68.4cm / 27인치', spec2: '200Hz / 평면',
     name: 'LG전자 울트라기어 27GS85Q',
     price: '568,000원',
   },
   {
-    id: 6,
+    id: "misc-6",
     img: 'lg_ultra.png', alt: '모니터6',
     spec1: '68.4cm / 27인치', spec2: '60Hz / 평면',
     name: 'LG전자 울트라HD 27US550',
@@ -49,21 +49,21 @@ const PRODUCTS = [
   },
   // 키보드
   {
-    id: 7,
+    id: "misc-7",
     img: 'dark.png', alt: '키보드1',
     spec1: '텐키리스 / 유선+무선', spec2: '기계식 / 키압:45g',
     name: 'darkFlash GD87 유무선 기계식',
     price: '36,630원',
   },
   {
-    id: 8,
+    id: "misc-8",
     img: 'abko.png', alt: '키보드2',
     spec1: '풀배열 / 유선', spec2: '기계식 / 키압:45g',
     name: '앱코 K560 축교환 레인보우 무빙 LED 기계식',
     price: '27,250원',
   },
   {
-    id: 9,
+    id: "misc-9",
     img: 'logi.png', alt: '키보드3',
     spec1: '풀배열 / 유선', spec2: '생활방수 / 키압:42g',
     name: '로지텍 K120 New',
@@ -71,21 +71,21 @@ const PRODUCTS = [
   },
   // 마우스
   {
-    id: 10,
+    id: "misc-10",
     img: 'raser.png', alt: '마우스1',
     spec1: '유선+무선 / 35000DPI', spec2: '가속도70G / 5버튼',
     name: 'Razer Viper V3 Pro',
     price: '239,000원',
   },
   {
-    id: 11,
+    id: "misc-11",
     img: 'logi_mouse.png', alt: '마우스2',
     spec1: '유선 / 8000DPI', spec2: '가속도30G / 5버튼',
     name: '로지텍 G102 LIGHTSYNC',
     price: '22,830원',
   },
   {
-    id: 12,
+    id: "misc-12",
     img: 'atk.png', alt: '마우스3',
     spec1: '유선+무선 / 42000DPI', spec2: '가속도50G / 5버튼',
     name: 'ATK A9 ULTRA 유무선 브라보텍',
--- my-app/src/pages/Notebooks.jsx
+++ my-app/src/pages/Notebooks.jsx
@@ -4,46 +4,46 @@ import "./notebooks.css";
 const IMG_BASE = "/laptops";
 const PRODUCTS = [ 
   // 삼성
-  { id:1,  img:"갤럭시_flex.png", alt:"노트북1",
+  { id:"notebook-1",  img:"갤럭시_flex.png", alt:"노트북1",
     spec1:"39.6cm / 1.55kg", spec2:"코어i7 / 54Wh 배터리",
     name:"삼성전자 터치노트북 PAN S NT950QCT-A58A (SSD 256GB)", price:"680,000원" },
-  { id:2,  img:"갤럭시북2.png", alt:"노트북2",
+  { id:"notebook-2",  img:"갤럭시북2.png", alt:"노트북2",
     spec1:"39.6cm / 1.8kg", spec2:"코어i5 / 45Wh",
     name:"삼성전자 갤럭시북2 NT550XEZ-A58A (SSD 256GB)", price:"698,000원" },
-  { id:3,  img:"갤럭시북4.png", alt:"노트북3",
+  { id:"notebook-3",  img:"갤럭시북4.png", alt:"노트북3",
     spec1:"35.6cm / 1.23kg", spec2:"코어 울트라5 / 65Wh",
     name:"삼성전자 갤럭시북4 프로 NT940XGQ-A51A WIN11 (SSD 512GB)", price:"1,559,000원" },
-  { id:4,  img:"갤럭시북5.png", alt:"노트북4",
+  { id:"notebook-4",  img:"갤럭시북5.png", alt:"노트북4",
     spec1:"35.6cm / 1.23kg", spec2:"코어 울트라7 / 65Wh",
     name:"삼성전자 갤럭시북5 프로 NT940XHA-K71AR (SSD 512GB)", price:"2,299,000원" },
 
   // LG
-  { id:5,  img:"lg_그램.png", alt:"노트북5",
+  { id:"notebook-5",  img:"lg_그램.png", alt:"노트북5",
     spec1:"43.1cm / 1.39kg", spec2:"코어 울트라5 / 65Wh",
     name:"LG전자 그램17 17ZD90SU-GXF6K(SSD 256GB)", price:"1,725,000원" },
-  { id:6,  img:"lg_울트라.png", alt:"노트북6",
+  { id:"notebook-6",  img:"lg_울트라.png", alt:"노트북6",
     spec1:"39.6cm / 1.7kg", spec2:"코어i5 / 65Wh",
     name:"LG전자 울트라PC 15UD50T-GX5JK(SSD 256GB)", price:"663,000원" },
 
   // MSI
-  { id:7,  img:"msi15.png", alt:"노트북7",
+  { id:"notebook-7",  img:"msi15.png", alt:"노트북7",
     spec1:"39.6cm / 1.9kg", spec2:"코어i5 / 42Wh",
     name:"MSI 모던시리즈 모던 15 H C13M-i5 (SSD 512GB)", price:"729,000원" },
-  { id:8,  img:"msi_소드.png", alt:"노트북8",
+  { id:"notebook-8",  img:"msi_소드.png", alt:"노트북8",
     spec1:"39.6cm / 2.25kg", spec2:"라이젠7 / 53.5Wh",
     name:"MSI 소드 GF66 A AI B8VE-R7 16GB램 (SSD 512GB)", price:"1,079,000원" },
 
   // 기타
-  { id:9,  img:"갤럭시북프로.png", alt:"노트북9",
+  { id:"notebook-9",  img:"갤럭시북프로.png", alt:"노트북9",
     spec1:"39.6cm / 1.39kg", spec2:"코어i5 / 68Wh",
     name:"삼성전자 갤럭시북 프로360 NT950QDY-A51A (SSD 256GB)", price:"1,490,000원" },
-  { id:10, img:"asus15.png", alt:"노트북10",
+  { id:"notebook-10", img:"asus15.png", alt:"노트북10",
     spec1:"39.6cm / 1.7kg", spec2:"라이젠7 / 42Wh",
     name:"ASUS 비보북 15 M1502YA-BQ635W (SSD 512GB)", price:"749,000원" },
-  { id:11, img:"asus_game.png", alt:"노트북11",
+  { id:"notebook-11", img:"asus_game.png", alt:"노트북11",
     spec1:"40.6cm / 1.95kg", spec2:"코어7 / 63Wh",
     name:"ASUS Gaming V16 V3607VM-RP005 (SSD 512GB)", price:"1,399,000원" },
-  { id:12, img:"asus_p1.png", alt:"노트북12",
+  { id:"notebook-12", img:"asus_p1.png", alt:"노트북12",
     spec1:"39.62cm / 1.64kg", spec2:"코어i5 / 65Wh",
     name:"ASUS ExpertBook P1 P1503CVA-S70532 (SSD 512GB)", price:"699,000원" }, ];
 
